502ccb0e0e06cd04787787fa159cdd661b61d947
ali-a321 <111758255+ali-a321@users.noreply.github.com>
2023-05-13 09:09:24 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
+const video = document.getElementById("myVideo");
+const playButton = document.getElementById("playButton");
 
-const videos = document.querySelectorAll('video');
-
-const observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.play();
-    } else {
-      entry.target.pause();
-    }
-  });
+playButton.addEventListener("click", function() {
+  video.play();
+  playButton.style.display = "none";
 });
 
-videos.forEach(video => {
-  observer.observe(video);
-});
+// const videos = document.querySelectorAll('video');
+
+// const observer = new IntersectionObserver((entries, observer) => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       entry.target.play();
+//     } else {
+//       entry.target.pause();
+//     }
+//   });
+// });
+
+// videos.forEach(video => {
+//   observer.observe(video);
+// });
 
 
-  //If i want play on hover instead
-  // document.querySelectorAll('.projectDemo').forEach(function(container) {
-  //   const video = container.querySelector('.videos');
-  //   container.addEventListener('mouseenter', function() {
-  //     video.play();
-  //   });
-  //   container.addEventListener('mouseleave', function() {
-  //     video.pause();
-  //   });
-  // });
+//   //If i want play on hover instead
+//   // document.querySelectorAll('.projectDemo').forEach(function(container) {
+//   //   const video = container.querySelector('.videos');
+//   //   container.addEventListener('mouseenter', function() {
+//   //     video.play();
+//   //   });
+//   //   container.addEventListener('mouseleave', function() {
+//   //     video.pause();
+//   //   });
+//   // });
 
